b5f06220af8a811a1e8f9ab83ed441853281e072
Maciej Łukasik <maciej.lukasik@valueadd.pl>
2022-03-27 21:18:15 +0200

    Prepare basic embedding and similarity models

---
+++ src/text_similarity_models.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+from src.embeddings import remove_punctuation, Embedding
+
+
+class TextSimilarityModel(ABC):
+
+    @abstractmethod
+    def get_similarity(self, a: str, b: str):
+        pass
+
+
+class JaccardModel(TextSimilarityModel):
+
+    def get_similarity(self, a: str, b: str):
+        a_s = set(remove_punctuation(a).split())
+        b_s = set(remove_punctuation(b).split())
+
+        intersection = a_s.intersection(b_s)
+        union = a_s.union(b_s)
+
+        return len(intersection) / len(union)
+
+
+class KMeansModel(TextSimilarityModel):
+
+    def get_similarity(self, a: str, b: str):
+        pass
+
+
+class CosineSimilarity(TextSimilarityModel):
+
+    def __init__(self, embedding: Embedding):
+        self.__embedding = embedding
+
+    def get_similarity(self, a: str, b: str):
+        e_a = self.__embedding.to_vec(remove_punctuation(a))
+        e_b = self.__embedding.to_vec(remove_punctuation(b))
+
+        np_e_a = np.array(e_a)
+        np_e_b = np.array(e_b)
+
+        return np.dot(np_e_a, np_e_b) / (np.sqrt(np.sum(np.power(np_e_a, 2))) * np.sqrt(np.sum(np.power(np_e_b, 2))))
+
+
+class LatentSemanticIndexing(TextSimilarityModel):
+
+    def get_similarity(self, a: str, b: str):
+        pass
+
+
+class WordMoverDistance(TextSimilarityModel):
+
+    def get_similarity(self, a: str, b: str):
+        pass
